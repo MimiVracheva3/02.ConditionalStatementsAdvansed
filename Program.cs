be2a7410aeea80d3618ca5f7e45dda791f0a8792
MimiVracheva3 <135064817+MimiVracheva3@users.noreply.github.com>
2023-06-10 13:47:02 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,21 +1,38 @@
 ﻿using System;
 
-namespace _02.GreaterNumber
+namespace _07.FigureArea
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            int a = int.Parse(Console.ReadLine());  
-            int b = int.Parse(Console.ReadLine());  
-
-            if (a>b)
+          string figure = Console.ReadLine();
+             
+         if (figure == "square")
             {
-                Console.WriteLine(a);
+                double sideA = double .Parse(Console.ReadLine());
+                double area = sideA * sideA;
+                Console.WriteLine($"{area:F3}");
             }
-            else
+         else if (figure == "rectangle")
             {
-                Console.WriteLine(b);
+                double sideA = double.Parse(Console.ReadLine());
+                double sideB = double.Parse(Console.ReadLine());    
+                double area = sideA * sideB;
+                Console.WriteLine($"{area:F3}");
+            }
+         else if (figure == "circle")
+            {
+                double radius = double.Parse(Console.ReadLine());
+                double area = Math.PI * Math.Pow(radius, 2);
+                Console.WriteLine($"{area:F3}");
+            }
+         else if (figure == "triangle")
+                {
+                double sideA = double.Parse(Console.ReadLine());
+                double hightA = double.Parse(Console.ReadLine());
+                double area = sideA * hightA / 2;
+                Console.WriteLine($"{area:F3}");
             }
         }
     }
